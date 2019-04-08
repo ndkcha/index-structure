15ccf01416cdcc6aabd7cc06003d0615513c0733
Anand Kacha <ndkcha@gmail.com>
2019-04-08 04:58:32 -0400

    Basic KD Tree

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
-public class Main {
-    public static void main(String[] args) {
+import kd.KdTree;
 
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        KdTree tree = new KdTree();
+        tree.buildIndex("data/LA2.txt");
     }
 }
